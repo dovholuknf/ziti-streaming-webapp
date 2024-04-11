dfa4000e78faeb97027a7ff8c2acaf5b9364994e
Jacob Dybvald Ludvigsen <contributions@ingeniorskap.no>
2024-04-11 14:35:08 +0200

    working SSE stream of images, using hypercorn for http/2 support

--- main.py
+++ main.py
@@ -1,67 +1,149 @@
 import openziti
-# import uvicorn
 from fastapi import FastAPI, Request
-from fastapi.responses import JSONResponse
-from fastapi.responses import StreamingResponse
-from fastapi.responses import FileResponse
+from fastapi.responses import HTMLResponse
 import asyncio
 from sse_starlette.sse import EventSourceResponse
-from fastapi.middleware import Middleware
-from fastapi.middleware.cors import CORSMiddleware
-from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
-import time
 
 from hypercorn.config import Config
 from hypercorn.asyncio import serve
 import signal
-
-# for hypercorn
-config = Config()
-config.bind = ["localhost:8443"]
-config.errorlog = "-" #stderr
-config.loglevel = "DEBUG"
-config.certfile = "local_cert_keep_secret.pem"
-config.keyfile = "local_key_keep_secret.pem"
+import cv2 as cv
+import base64
+import sys
 
 shutdown_event = asyncio.Event()
 
-middleware = [
-    Middleware(CORSMiddleware,
-               allow_origins=["*"],
-               allow_credentials=True,
-               allow_methods=["*"],
-               allow_headers=["*"],
-               )
-]
+# for hypercorn
+address = "localhost"
+port = 8443
+video_file = "lambs-and-cat.mp4" #change to local file
+identity = "./predalert_server2.json"
+service_name = "predalert_service_2"
 
-app = FastAPI(middleware=middleware)
+config = Config()
+config.bind = [f"{address}:{port}"]
+config.errorlog = "-" #stderr
+config.loglevel = "DEBUG"
+
+# Point these variables to  SSL cert/key files for HTTP/2 in hypercorn,
+# which is necessary to support more than 6 concurrent SSE connections per domain
+# config.certfile = "ssl_cert.pem"
+# config.keyfile = "ssl_key.pem"
 
 
-
-templates = Jinja2Templates(directory="static")
+app = FastAPI()
 
 
 @app.get("/")
 async def indexpage(request: Request):
-    return  templates.TemplateResponse(
-        request=request, name="sse-client.html")
+    html_content = """
+    <html>
+        <head>
+            <style>
+                #numbers {
+                    background-color: black;
+                    color:white;
+                    height:600px;
+                    overflow-x: hidden;
+                    overflow-y: auto;
+                    text-align: left;
+                    padding-left:10px;
+                }
+            </style>
+        </head>
+
+        <body>
+
+            <h1>Numbers:</h1>
+            <div id="numbers">
+            </div>
+            <script>
+                var source = new EventSource("/stream-sse");
+                source.onmessage = function(event) {
+                    document.getElementById("numbers").innerHTML += event.data + "<br>";
+                };
+            </script>
+
+        </body>
+    </html>
+    """
+    return HTMLResponse(content=html_content, status_code=200)
 
 
-async def message_generator(request):
+async def text_message_generator(request):
     for number in range(1, 256 + 1):
         if await request.is_disconnected():
             print("client disconnected!!!")
             break
         yield number
-        time.sleep(1)
+        await asyncio.sleep(0.1)
 
 
 @app.get('/stream-sse')
-async def message_stream(request: Request):
-    event_generator = message_generator(request)
+async def text_message_stream(request: Request):
+    event_generator = text_message_generator(request)
     return EventSourceResponse(event_generator)
 
+
+@app.get("/video")
+async def videopage(request: Request):
+    html_content = """
+    <html>
+        <head>
+            <title>SSE Image Streaming</title>
+        </head>
+
+        <body>
+            <img id="image" src="#" alt="Streamed Image" >
+            <script>
+                const imageElement = document.getElementById('image');
+                const eventSource = new EventSource('/vid-stream-sse'); // Replace '/stream' with your SSE endpoint
+
+                eventSource.onmessage = function(event) {
+                    const imageData = event.data;
+                    imageElement.src = 'data:image/webp;base64,' + imageData; // Change datatype if necessary, always ;base64
+                };
+
+                eventSource.onerror = function(error) {
+                    console.error('EventSource failed:', error);
+                    eventSource.close();
+                };
+            </script>
+
+        </body>
+    </html>
+    """
+    return HTMLResponse(content=html_content, status_code=200)
+
+
+async def vid_message_generator(request: Request):
+    stream = cv.VideoCapture(video_file)
+    # loop over frames
+    while True:
+        if await request.is_disconnected():
+            print("client disconnected!!!")
+            break
+        # read frame from provided source
+        (ret, frame) = stream.read()
+
+        # break if video is done
+        if not ret:
+            break
+
+        # handle JPEG encoding
+        _ , image = cv.imencode(".webp", frame)
+
+        # encode jpeg to base64 string in utf-8 format
+        base64_image = base64.b64encode(image).decode("utf-8")
+
+        yield base64_image
+        await asyncio.sleep(0.001)
+
+
+@app.get('/vid-stream-sse')
+async def vid_message_stream(request: Request):
+    event_generator = vid_message_generator(request)
+    return EventSourceResponse(event_generator)
 
 
 def _signal_handler(*_: any) -> None:
@@ -69,8 +151,8 @@ def _signal_handler(*_: any) -> None:
 
 
 @openziti.zitify(bindings={
-    ("localhost", 8443): {
-        'ztx': "./predalert_server2.json", 'service': "predalert_service_2"}
+    (address, port): {
+        'ztx': identity, 'service': service_name}
     })
 def run_webapp():
     loop = asyncio.get_event_loop()
@@ -81,4 +163,5 @@ def run_webapp():
 
 
 if __name__ == "__main__":
+
     run_webapp()
