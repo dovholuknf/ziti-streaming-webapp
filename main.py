e4a476108ca96a2774a772ba3bff0d21f4f39df1
Jacob Dybvald Ludvigsen <contributions@ingeniorskap.no>
2024-04-11 12:41:26 +0200

    working SSE stream of numbers, using hypercorn for http/2 support

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import openziti
-import uvicorn
+# import uvicorn
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 from fastapi.responses import StreamingResponse
@@ -12,6 +12,19 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 import time
 
+from hypercorn.config import Config
+from hypercorn.asyncio import serve
+import signal
+
+# for hypercorn
+config = Config()
+config.bind = ["localhost:8443"]
+config.errorlog = "-" #stderr
+config.loglevel = "DEBUG"
+config.certfile = "local_cert_keep_secret.pem"
+config.keyfile = "local_key_keep_secret.pem"
+
+shutdown_event = asyncio.Event()
 
 middleware = [
     Middleware(CORSMiddleware,
@@ -24,7 +37,6 @@ middleware = [
 
 app = FastAPI(middleware=middleware)
 
-from fastapi.templating import Jinja2Templates
 
 
 templates = Jinja2Templates(directory="static")
@@ -35,15 +47,6 @@ async def indexpage(request: Request):
     return  templates.TemplateResponse(
         request=request, name="sse-client.html")
 
-
-async def slow_numbers(minimum, maximum):
-    yield '<html><body><ul>'
-    for number in range(minimum, maximum + 1):
-        yield (b"--frame\r\nContent-Type:text/html\r\n\r\n" +
-               b"<li>" + number.to_bytes() + b"</li>\r\n")
-
-        await asyncio.sleep(0.5)
-    yield '</ul></body></html>'
 
 async def message_generator(request):
     for number in range(1, 256 + 1):
@@ -61,16 +64,20 @@ async def message_stream(request: Request):
 
 
 
+def _signal_handler(*_: any) -> None:
+    shutdown_event.set()
+
+
 @openziti.zitify(bindings={
-    ("127.0.0.1", 8443): {
+    ("localhost", 8443): {
         'ztx': "./predalert_server2.json", 'service': "predalert_service_2"}
     })
 def run_webapp():
-    uvicorn.run(
-        "main:app",
-        host='127.0.0.1',
-        port=8443
-        )
+    loop = asyncio.get_event_loop()
+    loop.add_signal_handler(signal.SIGTERM, _signal_handler)
+    loop.run_until_complete(
+        serve(app, config, shutdown_trigger=shutdown_event.wait)
+    )
 
 
 if __name__ == "__main__":
